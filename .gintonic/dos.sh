2fd2bd4dcde7ee6bc67161a175520abbb2ba7817
reda <reda@localhost>
2017-08-05 16:05:44 +0300

    launch dos in fullscreen

--- .gintonic/dos.sh
+++ .gintonic/dos.sh
@@ -5,8 +5,8 @@ shopt -s extglob
 exef=`ls $1/@(duke3d.exe|DOOM.EXE)`
 
 if [ -z "$exef" ]; then
-dosbox -exit $1/@(*.exe|*.EXE); # Run the firts exe
+dosbox $1/@(*.exe|*.EXE) -exit -fullscreen; # Run the firts exe
 else
-dosbox -exit "$exef";   # Run a preffered exe
+dosbox "$exef" -exit -fullscreen;   # Run a preffered exe
 fi
 
